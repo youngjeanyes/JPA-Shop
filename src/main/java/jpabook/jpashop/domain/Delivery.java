1826eb6aa97313588a5c0f01518ab013d1fa4f5e
youngjean <rateenyj@naver.com>
2024-04-21 22:01:35 +0900

    [Entity] Entity code commit

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+
+import static jakarta.persistence.FetchType.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @JsonIgnore
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; //READY, COMP
+}
---
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("B")
+@Getter
+@Setter
+public class Book extends Item {
+
+    private String author;
+    private String isbn;
+
+}
